2d7e9399ddd2307757e147169c5e11f9e48fd3d3
nikhilNathwani <nnathwani36@gmail.com>
2023-09-09 16:14:56 -0700

    Implemented coloring for squares in the blast zone! Very proud of this, very elegant react-based solution. Required passing hooks(?) for onMouseEnter and onMouseLeave for each Square, and changing the chompedSquares state variable from a 2D array of booleans to a 2D array of js enum values chomped/not-chomped/blast-zone

--- src/App.js
+++ src/App.js
@@ -5,59 +5,104 @@ import { useState } from "react";
 
 const numRows = 4;
 const numColumns = 5;
+const squareState = {
+	NOT_CHOMPED: 0,
+	CHOMPED: 1,
+	BLAST_ZONE: 2,
+};
 
 export default function Level() {
 	const [chompedSquares, setChompedSquares] = useState(
-		Array.from({ length: numRows }, () => Array(numColumns).fill(false))
+		Array.from({ length: numRows }, () =>
+			Array(numColumns).fill(squareState.NOT_CHOMPED)
+		)
 	);
 	const [playerOneIsNext, setPlayerOneIsNext] = useState(true);
-	let nextPlayerNumString = playerOneIsNext ? "1" : "2";
 
 	function handleChomp(row, col) {
 		let nextChompedSquares = chompedSquares.slice();
 		for (let i = row; i < numRows; i++) {
 			for (let j = col; j < numColumns; j++) {
-				nextChompedSquares[i][j] = true;
+				nextChompedSquares[i][j] = squareState.CHOMPED;
 			}
 		}
 		setChompedSquares(nextChompedSquares);
 		setPlayerOneIsNext(!playerOneIsNext);
 	}
 
+	function handleHoverChange(row, col, isHoverOn) {
+		if (chompedSquares[row][col] === squareState.CHOMPED) {
+			return;
+		}
+		let nextChompedSquares;
+		if (isHoverOn) {
+			nextChompedSquares = handleHoverOn(row, col);
+		} else {
+			nextChompedSquares = handleHoverOff(row, col);
+		}
+		setChompedSquares(nextChompedSquares);
+	}
+
+	function handleHoverOn(row, col) {
+		let nextChompedSquares = chompedSquares.slice();
+		for (let i = row; i < numRows; i++) {
+			for (let j = col; j < numColumns; j++) {
+				if (nextChompedSquares[i][j] !== squareState.CHOMPED) {
+					nextChompedSquares[i][j] = squareState.BLAST_ZONE;
+				}
+			}
+		}
+		return nextChompedSquares;
+	}
+
+	function handleHoverOff(row, col) {
+		let nextChompedSquares = chompedSquares.slice();
+		for (let i = row; i < numRows; i++) {
+			for (let j = col; j < numColumns; j++) {
+				if (nextChompedSquares[i][j] === squareState.BLAST_ZONE) {
+					nextChompedSquares[i][j] = squareState.NOT_CHOMPED;
+				}
+			}
+		}
+		return nextChompedSquares;
+	}
+
 	return (
 		<React.Fragment>
 			<p
 				id="nextTurnIndicator"
-				className={"player" + nextPlayerNumString}
+				className={"player" + (playerOneIsNext ? "-one" : "-two")}
 			>
-				Player {nextPlayerNumString}'s turn
+				Player {playerOneIsNext ? "1" : "2"}'s turn
 			</p>
 			<Board
 				chompedSquares={chompedSquares}
 				isPlayerOneNext={playerOneIsNext}
 				onChomp={handleChomp}
+				onHoverChange={handleHoverChange}
 			></Board>
 		</React.Fragment>
 	);
 }
 
-function Board({ chompedSquares, isPlayerOneNext, onChomp }) {
+function Board({ chompedSquares, isPlayerOneNext, onChomp, onHoverChange }) {
 	let board = chompedSquares.map((row, i) => (
 		<div
 			className={"board-row player-" + (isPlayerOneNext ? "one" : "two")}
 			key={i}
 		>
-			{row.map((isChomped, j) => {
+			{row.map((squareState, j) => {
 				return (
 					<Square
 						row={i}
 						col={j}
-						isChomped={isChomped}
+						state={squareState}
 						isPoison={i + j === 0}
 						onSquareClick={() => {
 							// console.log(`Clicked square at row ${i + 1}, column ${j + 1}`);
 							onChomp(i, j);
 						}}
+						onSquareHoverChange={onHoverChange}
 						key={(i + 1) * 10 + (j + 1)}
 					></Square>
 				);
@@ -67,11 +112,31 @@ function Board({ chompedSquares, isPlayerOneNext, onChomp }) {
 	return board;
 }
 
-function Square({ row, col, isChomped, isPoison, onSquareClick }) {
+function Square({
+	row,
+	col,
+	state,
+	isPoison,
+	onSquareClick,
+	onSquareHoverChange,
+}) {
 	return (
 		<div
-			className={"square " + (isChomped ? "chomped" : "notChomped")}
+			className={
+				"square " +
+				(state === squareState.CHOMPED
+					? "chomped"
+					: state === squareState.BLAST_ZONE
+					? "not-chomped blast-zone"
+					: "not-chomped")
+			}
 			onClick={onSquareClick}
+			onMouseEnter={() => {
+				onSquareHoverChange(row, col, true);
+			}}
+			onMouseLeave={() => {
+				onSquareHoverChange(row, col, false);
+			}}
 		>
 			{isPoison && (
 				<i className="fa-solid fa-skull-crossbones fa-2xl"></i>
