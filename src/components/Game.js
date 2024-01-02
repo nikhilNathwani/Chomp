c41a1dacb56512f6a6f8c76701c9450b92f1a62b
nikhilNathwani <nnathwani36@gmail.com>
2024-01-02 18:45:25 -0500

    Attempting to fix pre-selection bug on mobile 5

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,7 @@ import { squareState } from "../constants";
 import NextTurnIndicator from "./NextTurnIndicator";
 import ChocolateBar from "./ChocolateBar";
 
-let numRows = 6;
+let numRows = 4;
 let numColumns = 5;
 
 export default function Game() {
